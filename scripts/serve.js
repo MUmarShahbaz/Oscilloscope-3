d382af6a129a040669949d744f3c99e58465c2aa
M. Umar Shahbaz <m.umarshahbaz.2007@gmail.com>
2025-11-16 22:39:32 +0500

    Automated Serve

---
+++ scripts/serve.js
@@ -0,0 +1,15 @@
+import { create_child, quit_all } from './sub_process.cjs';
+import { existsSync } from 'fs';
+
+let children = [];
+process.on('SIGINT', () => quit_all(children));
+
+create_child('Jekyll', 'bundle exec jekyll serve', undefined, 10)
+        .then((child) => { children.push(child); })
+        .catch(e => console.log(e));
+
+const tailwind = () => {create_child('Tailwind', 'npx @tailwindcss/cli -o ./assets/css/tailwind.css', './_site', 10)
+        .then((child) => { children.push(child); })
+        .catch(e => console.log(e));}
+
+setInterval(async () => { if (!existsSync('./_site/assets/css/tailwind.css')) {tailwind()} }, 1000);
