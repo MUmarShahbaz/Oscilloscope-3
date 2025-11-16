23101bda244ffc3ac8f5b609366df80b7cfc3fee
M. Umar Shahbaz <m.umarshahbaz.2007@gmail.com>
2025-11-16 13:44:28 +0500

    Trying to add Tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: [
+    './_includes/**/*.html',
+    './_layouts/**/*.html',
+    './*.md',
+    './*.html',
+  ],
+  theme: {
+    theme: {
+      extend: {},
+    },
+  },
+  plugins: []
+}
