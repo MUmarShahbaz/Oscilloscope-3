18c70778533a6a5957576c39770bb8a42f1bf61a
M. Umar Shahbaz <m.umarshahbaz.2007@gmail.com>
2025-11-20 01:54:59 +0500

    stopped unnecessary tailwind triggering

--- scripts/serve.js
+++ scripts/serve.js
@@ -8,8 +8,10 @@ create_child('Jekyll', 'bundle exec jekyll serve', undefined, 10)
         .then((child) => { children.push(child); })
         .catch(e => console.log(e));
 
-const tailwind = () => {create_child('Tailwind', 'npx @tailwindcss/cli -i ./assets/css/tailwind.css -o ./_site/assets/css/tailwind.css', undefined , 10)
-        .then((child) => { children.push(child); })
-        .catch(e => console.log(e));}
+let tailwind_is_running = false;
 
-setInterval(async () => { if (!existsSync('./_site/assets/css/tailwind.css')) {tailwind()} }, 1000);
+const tailwind = () => { tailwind_is_running = true; create_child('Tailwind', 'npx @tailwindcss/cli -i ./assets/css/tailwind.css -o ./_site/assets/css/tailwind.css', undefined , 10)
+        .then((child) => { children.push(child); tailwind_is_running = false; })
+        .catch((e) => { console.log(e); tailwind_is_running = false; }); }
+
+setInterval(async () => { if (!existsSync('./_site/assets/css/tailwind.css') && !tailwind_is_running) {tailwind()} }, 1000);
